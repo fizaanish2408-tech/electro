4b11bac9393639ff381aeeb48bed961e65926f92
Fiza Anish <fizaanish24@gmail.com>
2026-02-14 23:00:16 +0530

    added live camera

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,53 @@
 
+"use client";
+
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isCameraOn, setIsCameraOn] = useState(false);
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      }
+
+      setIsCameraOn(true);
+    } catch (error) {
+      console.error(error);
+      alert("Camera access denied or not available.");
+    }
+  };
+
   return (
-     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
       <h1 className="text-4xl font-bold mb-6">
-        Electro-AI Hardware Assistant
+        Electro-AI Hardware Assistant ⚡
       </h1>
 
-      <p className="text-lg text-gray-400 mb-8 text-center max-w-xl">
-        Upload a circuit image and get instant AI-based explanation and troubleshooting help.
-      </p>
+      {!isCameraOn && (
+        <button
+          onClick={startCamera}
+          className="bg-white text-black px-6 py-3 rounded-xl font-semibold hover:bg-gray-200"
+        >
+          Open Live Camera
+        </button>
+      )}
 
-      <button className="bg-white text-black px-6 py-3 rounded-xl font-semibold hover:bg-gray-200">
-        Upload Circuit Image
-      </button>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        className={`mt-6 rounded-xl w-1/2 max-w-2xl border-4 border-white ${
+          isCameraOn ? "block" : "hidden"
+        }`}
+      />
     </main>
-
   );
 }
